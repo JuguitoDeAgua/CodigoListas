3143b6de58081cf67acb8d78362205c50b203492
JuguitoDeAgua <152459063+JuguitoDeAgua@users.noreply.github.com>
2023-11-30 00:03:57 -0400

    Update PRINCIPAL.java

--- ejemplo2multipleslistas/PRINCIPAL.java
+++ ejemplo2multipleslistas/PRINCIPAL.java
@@ -1,6 +1,3 @@
-
-package ejemplo2multipleslistas;
-
 public class PRINCIPAL {
     
     public static void main(String[] args) {
@@ -17,34 +14,34 @@ public class PRINCIPAL {
         
         LS_NormalCbPed pedi1 = new LS_NormalCbPed();
         pedi1.adiFinal(165, 23);
-        pedi1.adiFinal(231, 43);
-        pedi1.adiFinal(123, 55);
-        pedi1.adiFinal(165, 2);
+        //pedi1.adiFinal(231, 43);
+        //pedi1.adiFinal(123, 55);
+        //pedi1.adiFinal(165, 2);
         
         LS_NormalCbPed pedi2 = new LS_NormalCbPed();
         pedi2.adiFinal(345, 115);
-        pedi2.adiFinal(165, 78);
+        //pedi2.adiFinal(165, 78);
         
         LS_NormalCbPed pedi3 = new LS_NormalCbPed();
-        pedi3.adiFinal(231, 24);
+        //pedi3.adiFinal(231, 24);
         pedi3.adiFinal(165, 56);
-        pedi3.adiFinal(567, 67);
+        //pedi3.adiFinal(567, 67);
 
         
         LS_NormalOrd lor = new LS_NormalOrd();
         lor.adiFinal("Anthony Diaz", pedi1);
-        lor.adiFinal("Artux Creed", pedi2);
-        lor.adiFinal("Juanaa Simulator", pedi3);
+        //lor.adiFinal("Artux Creed", pedi2);
+        //lor.adiFinal("Juanaa Simulator", pedi3);
         
         LS_NormalOrd lor1 = new LS_NormalOrd();
         lor1.adiFinal("Anthony Diaz", pedi1);
-        lor1.adiFinal("Lusiana Creed", pedi2);
+        //lor1.adiFinal("Lusiana Creed", pedi2);
         
         LS_NormalOrd lor2 = new LS_NormalOrd();
         lor2.adiFinal("Anthonio Banderas", pedi1);
-        lor2.adiFinal("Pablo Creed", pedi2);
-        lor2.adiFinal("Elva Simulator", pedi3);
-        lor2.adiFinal("Robert Creed", pedi2);
+        //lor2.adiFinal("Pablo Creed", pedi2);
+        //lor2.adiFinal("Elva Simulator", pedi3);
+        //lor2.adiFinal("Robert Creed", pedi2);
         
         ven.adiFinal("23/nov/23", lor2);
         ven.adiFinal("25/dic/23", lor1);
@@ -59,6 +56,14 @@ public class PRINCIPAL {
         
         System.out.println("TOTAL GANANCIAS EN LA FECHA 25/dic/23:");
         System.out.println(TotalgananciasFecha(ven,comb,"25/dic/23"));
+        
+        System.out.println("**************//////////////////////////////************************************************");
+        
+        
+        mostrarTotalPedCombo(ven,comb);
+        
+        System.out.println("////////**************/////////////////");
+        CalculaPedidos(comb,ven);
     }
     
     public static double PrecioCombo(LS_NormalCb cb, int nrocb){
@@ -93,5 +98,80 @@ public class PRINCIPAL {
         }
         return total;
     }
+    // c. Por cada combo, calcular la cantidad total de pedidos de todas las fechas. 
+
+    public static boolean BuscaNroCombo(LS_NormalCb cb ,int nroCB){
+        NodoCb R = cb.getP();
+        boolean sw =false;
+        while(R!=null){
+            if(R.getNroCombo()==nroCB){
+                //return R.getNroCombo();
+                sw=true;
+                return sw;
+            }
+            R = R.getSig();
+        }
+        return sw;
+        //return 0;
+    }
     
+    public static void CalculaPedidos(LS_NormalCb cbb, LS_NormalVenta ven){
+        NodoVenta R = ven.getP();
+        int canti = 0;
+        while(R != null){
+            NodoOrd Q = R.getLord().getP();
+            while(Q !=null){
+                NodoCbPed S = Q.getPed().getP();
+                while(S!=null){
+                    int kk = S.getNroCombo();
+                    if(BuscaNroCombo(cbb,kk)){
+                        canti = S.getCantidad()+canti;
+                        System.out.println("total pedidos del combo:"+canti);
+                    }
+                    S = S.getSig();
+                }
+                Q = Q.getSig();
+            }
+            R = R.getSig();
+        }
+        
+        System.out.println("cantidad: "+canti);
+    }
+  
+    
+    public static int contarPedCombo(LS_NormalVenta lsv, int nroCombox) {
+        int cant = 0;
+        NodoVenta R = lsv.getP();
+
+        while (R != null) {
+            NodoOrd Q = R.getLord().getP();
+
+            while (Q != null) {
+                NodoCbPed S = Q.getPed().getP();
+
+                while (S != null) {
+                    if (S.getNroCombo() == nroCombox) {
+                        cant += S.getCantidad();
+                    }
+                    S = S.getSig();
+                }
+
+                Q = Q.getSig();
+            }
+
+            R = R.getSig();
+        }
+
+        return cant;
+    }
+
+    public static void mostrarTotalPedCombo(LS_NormalVenta lsv, LS_NormalCb lsc) {
+        NodoCb R = lsc.getP();
+
+        while (R != null) {
+            int nroCombo = R.getNroCombo();
+            System.out.println(R.getNombre() + " " + contarPedCombo(lsv, nroCombo));
+            R = R.getSig();
+        }
+    }
 }
